2fcb317dc01a702da4f5397f3036892de7801c3f
summer0207 <5735993@hanmail.net>
2023-04-19 13:11:35 +0900

    master

--- java2/src/Network/Net2.java
+++ java2/src/Network/Net2.java
@@ -1,5 +1,13 @@
 package Network;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Net2 {
 	//PORT 접속을 하고자하는 경로
 	/* 
@@ -29,7 +37,50 @@ public class Net2 {
 	}
 }
 class getimage{
+	Scanner sc = null;
+	InputStream is = null;
+	BufferedInputStream bi = null;
+	FileOutputStream fs = null;
+	
 	public void img() throws Exception {
+		this.sc = new Scanner(System.in);
+		System.out.println("웹에서 가져올 이미지 주소를 입력해 주세요?");
+		String url = sc.nextLine();
 		
+		URL u = new URL(url);	//URL(클래스) : 네트워크 경로를 말합니다. (절대경로 형태로 적용)
+		URLConnection con = u.openConnection();	//해당 경로를 연결하는 것을 말함
+		System.out.println(con);
+		
+		int imgsize = con.getContentLength();	//이미지 사이즈
+		System.out.println(imgsize);
+		String imgtype = con.getContentType();	//이미지 타입형태
+		System.out.println(imgtype);
+		//long date = con.getDate();
+		String date = String.valueOf(con.getDate());	//업로드 날짜
+		
+		this.is = u.openStream();	//통신에서는 openStream만 사용가능
+		this.bi = new BufferedInputStream(this.is);	//이미지 파일을 임시로 메모리에 저장
+		//byte data[] = new byte[this.bi.available()];
+		byte data[] = new byte[1024];	//1KB = 1024 해당 파일을 조각냄
+		
+		Date today = new Date();
+		String Filename = today.getDate()+""+today.getTime()+".png";
+		
+		//파일 이름을 해당 경로에 생성만 함
+		this.fs = new FileOutputStream("E:\\memo\\"+Filename);
+		
+		int imgdata = 0;
+		int count = 0;
+		while((imgdata=this.bi.read(data))!=-1) {
+			this.fs.write(data,0,imgdata);	//조각난 파일을 취합하여 저장시킴
+			count += imgdata;
+			System.out.println("다운로드 중..."+ (count*100)/imgsize + "%");
+		}
+		System.out.println("다운로드 완료 되었습니다.");
+		this.fs.flush();	//메모리를 비움
+		this.fs.close();
+		this.sc.close();
+		this.is.close();
+		this.bi.close();
 	}
 }
