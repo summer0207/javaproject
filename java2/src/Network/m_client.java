f73b3c9e66d8b797bfb9cbf9d18f8a6e5c45f3e3
summer0207 <5735993@hanmail.net>
2023-04-20 18:22:31 +0900

    master

--- java2/src/Network/m_client.java
+++ java2/src/Network/m_client.java
@@ -29,15 +29,28 @@ class mchat{
 	private String msg = "";	//메세지 출력
 	
 	public mchat(String id) {
+		
 		try {
-			this.so = new Socket(this.ip,this.port);
-			this.is = this.so.getInputStream();
-			this.os = this.so.getOutputStream();
 			this.msg = "WELCOME " + id;
-			this.os.write(this.msg.getBytes());
 			
-			this.sc = new Scanner(System.in);
+			while(true) {
+				this.so = new Socket(this.ip,this.port);
+				this.is = this.so.getInputStream();
+				this.os = this.so.getOutputStream();
 			
+				this.sc = new Scanner(System.in);
+				System.out.println("chat : ");
+				this.msg = this.sc.nextLine();
+			
+				this.os.write(this.msg.getBytes());
+				this.os.flush();
+				
+				byte data[] = new byte[4096];
+				int n = this.is.read(data);
+				this.msg = new String(data,0,n);
+				System.out.println(this.msg);
+				
+			}	
 		} 
 		catch (Exception e) {
 			System.out.println("Server Connect Error!!");
--- java2/src/Network/m_server.java
+++ java2/src/Network/m_server.java
@@ -59,20 +59,23 @@ class chat_thread extends Thread{
 	@Override
 	public void run() {
 		try {
-			byte data[] = new byte[1024];
-			int n = this.is.read(data);
-			this.msg = new String(data,0,n);
-			System.out.println(this.msg);
 			while(true) {
-				this.sc = new Scanner(System.in);
-				System.out.println("message: ");
-				String result = this.sc.nextLine();
+				byte data[] = new byte[1024];
+				int n = this.is.read(data);
+				this.msg = new String(data,0,n);
+				System.out.println(this.msg);
+				//this.sc = new Scanner(System.in);
+				//System.out.println("message: ");
+				String result = this.msg;
+				
+				/* 한명이 입력한 내용을 접속한 모든 사용자에게 메세지를 전달하기 위함 */
 				int f;
 				for(f=0; f<this.user.size(); f++) {
 					this.os = this.user.get(f).getOutputStream();
 					this.os.write(result.getBytes());
 					this.os.flush();
 				}
+				/* 전송 끝 */
 			}
 			
 		}
