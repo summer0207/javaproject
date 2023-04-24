a6bcf6d9dbf525fcb606a197759289f96fc2debc
summer0207 <5735993@hanmail.net>
2023-04-24 13:03:35 +0900

    master

--- java2/src/Network/multi_client.java
+++ java2/src/Network/multi_client.java
@@ -3,7 +3,7 @@ package Network;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -20,21 +20,23 @@ public class multi_client {
 		InputStreamReader isr = null;
 		Scanner sc = null;
 		try {
+			//서버 접속 ip 및 포트 설정
 			sk = new Socket("192.168.10.142",10001);
 			System.out.println("Connect Success");
 			sc = new Scanner(System.in);
-			System.out.println("user id: ");
+			System.out.println("user id: ");	//사용자 아이디
 			String name = sc.next();
+			//멀티 스레드로 사용자 아이디 및 소켓 전달
 			Thread tr = new ch_client(sk, name);
-			tr.start();
+			tr.start();	//멀티 스레드 가동
 			
-			is = sk.getInputStream();
-			isr = new InputStreamReader(is);
-			br = new BufferedReader(isr);
+			is = sk.getInputStream();	//읽기
+			isr = new InputStreamReader(is);	//byte -> String
+			br = new BufferedReader(isr);	//메모리에 저장
 			
 			while(br != null) {
-				String msg = br.readLine();
-				System.out.println("MSG:"+ msg);
+				String msg = br.readLine();	//메모리 문자 확인
+				System.out.println(msg);	//메세지 전달
 			}
 		} 
 		catch (Exception e) {
@@ -47,6 +49,8 @@ class ch_client extends Thread{
 	Socket sk = null;
 	String name = null;
 	Scanner sc = null;
+	
+	//소켓이랑 아이디 값을 기본설정으로 등록
 	public ch_client(Socket s, String name) {	//setter
 		this.sk = s;
 		this.name = name;
@@ -55,15 +59,22 @@ class ch_client extends Thread{
 	@Override
 	public void run() {
 		try {
-			PrintStream ps = new PrintStream(this.sk.getOutputStream());
-			ps.println(this.name);
+			//byte -> String으로 변환
+			PrintWriter ps = new PrintWriter(this.sk.getOutputStream());
+			ps.println(this.name);	//입장시 1회만 사용
 			ps.flush();
 			
-			while(true) {
+			while(true) {	//사용자가 입력하는 메세지를 전달
 				this.sc = new Scanner(System.in);
 				String msg = this.sc.nextLine();
-				ps.println(msg);
-				ps.flush();
+				ps.println(msg);	//전달 메세지 출력
+				ps.flush();	//메세지 초기화
+				if(msg.equals("exit")) {
+					System.out.println("chatting end...");
+					System.exit(0);	//프로세서 종료
+					this.sc.close();
+					break;
+				}
 			}
 		} 
 		catch (Exception e) {
--- java2/src/Network/multi_server.java
+++ java2/src/Network/multi_server.java
@@ -1,7 +1,9 @@
 package Network;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,15 +16,15 @@ public class multi_server {
 		ms.info();
 	}
 	public void info() {
-		ServerSocket ss = null;
-		Socket sk = null;
+		ServerSocket ss = null;	//서버 소켓 클래스 로드
+		Socket sk = null;	//소켓 클래스 로드
 		try {
-			ss = new ServerSocket(10001);
-			while(true) {
+			ss = new ServerSocket(10001);	//서버 포트 오픈
+			while(true) {	//사용자가 추가 될 때 마다 멀티스레드가 작동 
 				System.out.println("Chroom Start");
-				sk = ss.accept();
-				ch_server ch = new ch_server(sk);
-				ch.start();
+				sk = ss.accept();	//연결 유지 (반복문 필수)
+				ch_server ch = new ch_server(sk);	//즉시실행 메소드 소켓 전달
+				ch.start();	//멀티 스레드로 전달
 			}
 		} 
 		catch (Exception e) {
@@ -32,45 +34,62 @@ public class multi_server {
 }
 
 class ch_server extends Thread{
+	//사용자 정보를 저장하는 부분 (메모리에 무조건 등록) - 미등록시 초기화됨
 	static ArrayList<PrintWriter> user = new ArrayList<>();
+	
 	Socket sk = null;
 	BufferedReader br = null;
 	PrintWriter pw = null;
 	InputStreamReader isr = null;		
+	OutputStream os = null;
+	InputStream is = null;
 	
-	public ch_server(Socket s) {
-		this.sk = s;
-		try {
-			this.pw = new PrintWriter(this.sk.getOutputStream());
-			this.br = new BufferedReader(new InputStreamReader(this.sk.getInputStream()));
-			user.add(this.pw);
+	public ch_server(Socket s) {	//setter 기본 옵션을 모두 셋팅
+		this.sk = s;	//소켓을 필드값으로 전달
+		try {	//모든 필드값에 속성값을 전달
+			this.os = this.sk.getOutputStream();	//쓰기
+			this.pw = new PrintWriter(this.os);
+			
+			this.is = this.sk.getInputStream();	//읽기 (byte)
+			this.isr = new InputStreamReader(this.is);	//byte -> String 변경
+			this.br = new BufferedReader(this.isr);	//메모리로 저장
+			
+			user.add(this.pw);	//배열에 사용자를 추가
 		} 
 		catch (Exception e) {
-			System.out.println("Soceket Error");
+			System.out.println("Socket Error");
 		}
 	}
 	
 	@Override
-	public void run() {
+	public void run() {	//스레드 작동
 		String name = "";
 		try {
 			name = this.br.readLine();
-			System.out.println("["+name+"]");
-			info("["+name+"] Welcome");
+			//System.out.println("["+name+"]");	//접속자 사항
+			info("["+name+"] Welcome");	//클라이언트 환영 메세지 출력 1회(최초만)
 			
-			while(this.br != null) {
+			while(this.br != null) {	//클라이언트가 입력한 메세지를 전달(배열에 있는 모든 사용자에게)
 				String inmsg = this.br.readLine();
+				
+				if(inmsg.equals("exit")) {	//클라이언트 exit 입력시 
+					user.remove(this.pw);	//사용자 삭제
+					info(name+"[OUT]");	//사용자들에게 접속을 종료한 아이디를 출력
+					break;	//반복문 종료
+				}
+				else {
 				info(name + " : " + inmsg);
+				}
 			}
 		} 
 		catch (Exception e) {
 			System.out.println("User id Error");
 		}
 	}
-	public void info(String s) {
-		for(PrintWriter p : user) {
-			p.println(s);
-			p.flush();
+	public void info(String s) {	//전달 형태
+		for(PrintWriter p : user) {	//접속한 사용자 전체 리스트 반복
+			p.println(s);	//메세지 전달
+			p.flush();	//메모리 메세지 초기화
 		}
 	}
 }
